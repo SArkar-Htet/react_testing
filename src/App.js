f4d578cd03bb18422f33de92da46e690857a9dfa
SArkar-Htet <dev.soearkarhtet@gmail.com>
2020-01-19 22:28:55 +0630

    Hello Hnyun

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <p>Hello Hnyun Yadanar Myintmo Htun</p>
       </header>
     </div>
   );
